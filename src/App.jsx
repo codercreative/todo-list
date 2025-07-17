4e8d8a65f30262d97fcd6c53a688d93a64019494
Christina Ligare <cligare@hotmail.com>
2025-07-17 09:36:04 -0700

    update directories again and update labelText props

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import './App.css';
 import TodoForm from './features/TodoForm.jsx';
-import TodoList from './features/TodoList.jsx';
+import TodoList from './features/TodoList/TodoList.jsx';
 
 function App() {
   const [todoList, setTodoList] = useState([]);
